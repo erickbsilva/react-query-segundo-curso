291ac98fbf55ea8bd37d1f483c44c045e465a6e1
erick <erickbarretodasilva@gmail.com>
2026-03-31 19:10:16 -0300

    uso do prefetching para melhorar a latência da resposta

--- src/app/hooks/useFetchReplies.js
+++ src/app/hooks/useFetchReplies.js
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 
-const fetchReplies = async ({ commentId, slug }) => {
+export const fetchReplies = async ({ commentId, slug }) => {
   const response = await fetch(`/api/comment/${commentId}/replies?slug=${slug}`);
   if (!response.ok) {
     throw new Error("A resposta de rede não está ok");
@@ -11,8 +11,9 @@ const fetchReplies = async ({ commentId, slug }) => {
 // enabled: !!commentId && !!slug só vai ser executado se commentId e slug forem verdadeiros
 export const useFetchReplies = ({ commentId, slug }) => {
   return useQuery({
-    queryKey: ["replies", commentId],
+    queryKey: ["replies", commentId, slug],
     queryFn: async () => fetchReplies({ commentId, slug }),
     enabled: !!commentId && !!slug,
+    retry: 5,
   });
 };
--- src/components/Replies/index.jsx
+++ src/components/Replies/index.jsx
@@ -5,11 +5,35 @@ import styles from "./replies.module.css";
 import { Comment } from "../Comment";
 import { ReplyModal } from "../ModalReply";
 import { useFetchReplies } from "@/app/hooks/useFetchReplies";
+import { fetchReplies } from "@/app/hooks/useFetchReplies";
+import { useQueryClient } from "@tanstack/react-query";
 
 export const Replies = ({ comment, slug }) => {
+
+  const queryClient = useQueryClient();
+
   const [showReplies, setShowReplies] = useState(false);
 
   const { data: replies } = useFetchReplies(showReplies ? { commentId: comment.id, slug } : {});
+
+
+/**
+ * Prefetch the replies data when the user hovers over the "Ver" button.
+ * This function is called when the user hovers over the button and it prefetches the data
+ * using the `useQueryClient` hook from `@tanstack/react-query`.
+ * If the data is already fetched, it will not prefetch again.
+ */
+// o prefetch serve para pre carregar os dados da api quando o usuario passar o mouse por cima antes de carregar o comentário, por causa da lentidão de abrir o reply do usuário
+  const prefetch = () => {
+    if (!showReplies) {
+      queryClient.prefetchQuery({
+        queryKey: ["replies", comment.id, slug],
+        queryFn: () => fetchReplies({ commentId: comment.id, slug }),
+        retry: 5,
+        retryDelay: 500,
+      });
+    }
+  };
 
   return (
     <div className={styles.container}>
@@ -17,6 +41,7 @@ export const Replies = ({ comment, slug }) => {
         <button
           className={styles.btn}
           onClick={() => setShowReplies(!showReplies)}
+          onMouseOver={prefetch}
         >
           {showReplies ? "Ocultar" : "Ver"} respostas
         </button>
